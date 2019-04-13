5f32d489fffc76c5d9c9b668b74bee443525d3a7
Yuri Pourre <yuripourre@gmail.com>
2019-04-13 14:33:00 -0300

    WIP

---
+++ examples/sphereflake.java
@@ -0,0 +1,11 @@
+import org.sunflow.core.*;
+import org.sunflow.core.accel.*;
+import org.sunflow.core.camera.*;
+import org.sunflow.core.primitive.*;
+import org.sunflow.core.shader.*;
+import org.sunflow.image.Color;
+import org.sunflow.math.*;
+
+public void build() {
+  include("/home/yuri/git/sunflow/examples/sphereflake.sc");
+}
--- src/main/java/examples/MeshExample.java
+++ src/main/java/examples/MeshExample.java
@@ -6,9 +6,10 @@ import org.sunflow.core.parameter.InstanceParameter;
 import org.sunflow.core.parameter.PhotonParameter;
 import org.sunflow.core.parameter.TraceDepthsParameter;
 import org.sunflow.core.parameter.camera.PinholeCameraParameter;
-import org.sunflow.core.parameter.geometry.SphereParameter;
+import org.sunflow.core.parameter.geometry.*;
 import org.sunflow.core.parameter.gi.InstantGIParameter;
 import org.sunflow.core.parameter.light.CornellBoxLightParameter;
+import org.sunflow.core.parameter.shader.DiffuseShaderParameter;
 import org.sunflow.core.parameter.shader.GlassShaderParameter;
 import org.sunflow.core.parameter.shader.MirrorShaderParameter;
 import org.sunflow.image.Color;
@@ -16,17 +17,17 @@ import org.sunflow.math.Matrix4;
 import org.sunflow.math.Point3;
 import org.sunflow.math.Vector3;
 
-public class CornellBoxJensenScene {
+public class MeshExample {
 
     public static void main(String[] args) {
         SunflowAPI api = new SunflowAPI();
         api.reset();
 
         ImageParameter image = new ImageParameter();
-        image.setResolutionX(800);
-        image.setResolutionY(600);
+        image.setResolutionX(400);
+        image.setResolutionY(300);
         image.setAAMin(0);
-        image.setAAMax(2);
+        image.setAAMax(1);
         image.setFilter(ImageParameter.FILTER_GAUSSIAN);
         image.setup(api);
 
@@ -56,8 +57,8 @@ public class CornellBoxJensenScene {
         Point3 eye = new Point3(0, -205, 50);
         Point3 target = new Point3(0, 0, 50);
         Vector3 up = new Vector3(0, 0, 1);
-
-        camera.setupTransform(api, eye,target,up);
+        // TODO Move logic to camera
+        api.parameter("transform", Matrix4.lookAt(eye, target, up));
 
         camera.setFov(45f);
         camera.setAspect(1.333333f);
@@ -87,19 +88,64 @@ public class CornellBoxJensenScene {
         lightParameter.setSamples(32);
         lightParameter.setup(api);
 
+        InstanceParameter mirrorSphereInstance = new InstanceParameter();
+        mirrorSphereInstance.shaders("Mirror");
+
         SphereParameter mirrorSphere = new SphereParameter();
         mirrorSphere.setName("mirror-sphere");
-        mirrorSphere.setCenter(new Point3(-30, 30, 20));
-        mirrorSphere.setInstanceParameter(new InstanceParameter().shaders("Mirror"));
+        mirrorSphere.setCenter(new Point3(30, 30, 20));
+        mirrorSphere.setInstanceParameter(mirrorSphereInstance);
         mirrorSphere.setRadius(20);
+        // Hide Sphere
         mirrorSphere.setup(api);
 
+        SphereParameter otherSphere = new SphereParameter();
+        otherSphere.setName("other-sphere");
+        otherSphere.setCenter(new Point3(-30, 30, 50));
+        otherSphere.setInstanceParameter(mirrorSphereInstance);
+        otherSphere.setRadius(20);
+        // Hide Sphere
+        otherSphere.setup(api);
+
+        DiffuseShaderParameter simpleYellow = new DiffuseShaderParameter("simple_yellow");
+        simpleYellow.setDiffuse(new Color(0.70f, 0.70f, 0.15f).toNonLinear());
+        simpleYellow.setup(api);
+
+        SphereFlakeParameter sphereFlakeParameter = new SphereFlakeParameter("left");
+        sphereFlakeParameter.setInstanceParameter(new InstanceParameter().shaders("simple_yellow"));
+        sphereFlakeParameter.setRadius(20);
+        sphereFlakeParameter.setLevel(6);
+        sphereFlakeParameter.setup(api);
+
+        /*InstanceParameter glassSphereInstance = new InstanceParameter();
+        glassSphereInstance.setShaders(new String[]{"Glass"});
         SphereParameter glassSphere = new SphereParameter();
         glassSphere.setName("glass-sphere");
         glassSphere.setCenter(new Point3(28, 2, 20));
-        glassSphere.setInstanceParameter(new InstanceParameter().shaders("Glass"));
+        glassSphere.setInstanceParameter(glassSphereInstance);
         glassSphere.setRadius(20);
-        glassSphere.setup(api);
+        glassSphere.setup(api);*/
+
+        float y = 30;
+
+        PlaneParameter plane = new PlaneParameter();
+        plane.setName("my_plane");
+        plane.setCenter(new Point3(28,30,20));
+        plane.setPoint1(new Point3(300,60,50));
+        plane.setPoint2(new Point3(0,0,-1));
+        plane.setNormal(new Vector3(0, 0, 1));
+        //plane.setInstanceParameter(mirrorSphereInstance);
+        plane.setup(api);
+
+        GenericMeshParameter mesh = new GenericMeshParameter();
+        mesh.setName("triangularMesh");
+        mesh.setPoints(new float[]{-30, y, 0, 0, y, 100, 500, y, 0});
+        mesh.setUvs(new float[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+        mesh.setTriangles(new int[]{0, 1, 2});
+        mesh.setNormals(new float[]{0, -1, 0, 0, -1, 0, 0, -1, 0});
+
+        mesh.setInstanceParameter(mirrorSphereInstance);
+        mesh.setup(api);
 
         finalRender(api);
     }
